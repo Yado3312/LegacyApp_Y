c8033cad1af14c18e35408eda5606e8cbd167bc3
Yado3312 <goyado4@gmail.com>
2026-01-29 18:43:02 -0600

    FUNCIONA DIOSITO POR FAAVOR TE LO PIDO TE AMO QUIERO UNA NOVIA

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import authRoutes from './routes/auth.routes.js';
 import userRoutes from './routes/user.routes.js';
@@ -11,9 +13,15 @@ import reportRoutes from './routes/report.routes.js';
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.use(express.static(path.join(__dirname, 'frontend')));
+
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
@@ -24,9 +32,5 @@ app.use('/api/tasks', taskRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/reports', reportRoutes);
-
-app.get('/', (req, res) => {
-  res.json({ status: 'API running' });
-});
 
 export default app;
