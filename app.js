9807802d149b4d538d5d6862cfaeab44eb0e2efa
Yado3312 <goyado4@gmail.com>
2026-01-29 18:51:18 -0600

    VUELVE A MI ALO TE EXTRANO

--- app.js
+++ app.js
@@ -13,16 +13,25 @@ import reportRoutes from './routes/report.routes.js';
 
 const app = express();
 
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// ==============================
+// CORS (ANTES de las rutas)
+// ==============================
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
-app.use(express.static(path.join(__dirname, 'frontend')));
 
-
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 
 app.use('/api/auth', authRoutes);
@@ -32,5 +41,6 @@ app.use('/api/tasks', taskRoutes);
 app.use('/api/comments', commentRoutes);
 app.use('/api/notifications', notificationRoutes);
 app.use('/api/reports', reportRoutes);
+
 
 export default app;
